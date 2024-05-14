4d7c298726136cc478e437450eb676e0e7380e39
Bronte Mckeown <bronte.mckeown@gmail.com>
2024-05-14 21:22:03 +0100

    Create correlation.py script to correlate two made-up variables using panda's corr() function.

---
+++ correlation.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+## Create two pandas series
+
+# Create x using range() function
+x = pd.Series(range(20, 30))
+# Create y manually
+y = pd.Series([3, -1, 7, 9, 8, -14, 18, 27, 61, 32])
+
+# Run Pearson correlation (default) on two variables
+# Store in 'result' variable
+result = x.corr(y)
+
+# Print result to console
+print(result)
