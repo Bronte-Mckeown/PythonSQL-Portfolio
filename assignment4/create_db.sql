76e8104a5ee32457a96404ef1eb0a4866b626b69
Bronte Mckeown <bronte.mckeown@gmail.com>
2024-06-30 11:16:42 +0100

    Create nails database with nail bookings table

---
+++ assignment4/create_db.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE nails;
+USE nails;
+
+CREATE TABLE `nail_bookings` (
+  `nailTech` varchar(45) DEFAULT NULL,
+  `appointmentType` varchar(45) DEFAULT NULL,
+  `12-13` char(6) DEFAULT NULL,
+  `12-13-client` varchar(20) DEFAULT NULL,
+  `13-14` char(6) DEFAULT NULL,
+  `13-14-client` varchar(20) DEFAULT NULL,
+  `14-15` char(6) DEFAULT NULL,
+  `14-15-client` varchar(20) DEFAULT NULL,
+  `15-16` char(6) DEFAULT NULL,
+  `15-16-client` varchar(20) DEFAULT NULL,
+  `16-17` char(6) DEFAULT NULL,
+  `16-17-client` varchar(20) DEFAULT NULL,
+  `17-18` char(6) DEFAULT NULL,
+  `17-18-client` varchar(20) DEFAULT NULL,
+  `bookingDate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Stored procedure to insert rows into nail bookings table.
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `filldates`(dateStart DATE, dateEnd DATE, nailTech VARCHAR(45))
+BEGIN
+-- While date start is less than date end, perform loop to insert empty rows.
+  WHILE dateStart <= dateEnd DO
+	-- Insert new record into booking table.
+    INSERT INTO nail_bookings (nailTech, bookingDate) VALUES (nailTech, dateStart);
+    -- Adds 1 day to datestart before start of next loop.
+    SET dateStart = date_add(dateStart, INTERVAL 1 DAY);
+-- End loop. 
+  END WHILE;
+-- End procedure. 
+END$$
+DELIMITER ;
+
+CALL `nails`.`filldates`(20240629, 20240930, 'Bronte');
+CALL `nails`.`filldates`(20240629, 20240930, 'Finn');
+CALL `nails`.`filldates`(20240629, 20240930, 'Max');
+
+SELECT * FROM nail_bookings;
+
