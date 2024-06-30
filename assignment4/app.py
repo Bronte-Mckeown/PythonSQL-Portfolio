ff78e0a406675a0ff81e8408708edd2ba12ac007
Bronte Mckeown <bronte.mckeown@gmail.com>
2024-06-30 20:34:24 +0100

    added app methods

--- assignment4/app.py
+++ assignment4/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from db_utils import get_all_booking_availability, add_booking
+from db_utils import get_all_booking_availability, get_nailTech_availability, add_booking, delete_booking
 
 # Create flask instance (i.e., API) and save to app variable
 app = Flask(__name__)
@@ -7,14 +7,23 @@ app = Flask(__name__)
 # First thing is to create first end point to create landing page
 @app.route('/') # this creates the URL
 def get_landingpage():
-    return "Welcome to Our Nail Salon. Please go to our availability page to see available appointments."
+    return "Landing page for nail salon API."
 
-@app.route('/availability/<date>')
+# Uses method 'GET' to get availability based on date
+@app.route('/date_availability/<date>')
 def get_bookings(date):
     # use function from db_utils to get booking availability
     res = get_all_booking_availability(date) 
     return jsonify(res)
 
+# Uses method 'GET' to get availability based on nail tech name
+@app.route('/tech_availability/<nailTech>')
+def get_nailTech_bookings(nailTech):
+    # use function from db_utils to get availability of nail tech
+    res = get_nailTech_availability(nailTech)
+    return jsonify(res)
+
+# Used method 'PUT' to add booking
 @app.route('/booking', methods=['PUT'])
 def book_appt():
     booking = request.get_json()
@@ -23,7 +32,19 @@ def book_appt():
         nailTech=booking['nailTech'],
         appointmentType=booking['appointmentType'],
         time=booking['time'],
-        customer=booking['customer']
+        client=booking['client'],
+        contact=booking['contact']
+    )
+    return jsonify(booking)
+
+# Used method 'PUT' to delete booking
+@app.route('/delete', methods=['PUT'])
+def delete_appt():
+    booking = request.get_json()
+    delete_booking(
+        _date=booking['_date'],
+        time=booking['time'],
+        contact=booking['contact']
     )
     return jsonify(booking)
 
