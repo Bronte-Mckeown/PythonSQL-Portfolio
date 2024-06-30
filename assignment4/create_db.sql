4ab93ac531632f899aa79a1a7a0516590b799db5
Bronte Mckeown <bronte.mckeown@gmail.com>
2024-06-30 11:49:51 +0100

    Updated database
    - Time slot column now accepts appointment type, and only from specific values.

--- assignment4/create_db.sql
+++ assignment4/create_db.sql
@@ -3,19 +3,24 @@ USE nails;
 
 CREATE TABLE `nail_bookings` (
   `nailTech` varchar(45) DEFAULT NULL,
-  `appointmentType` varchar(45) DEFAULT NULL,
-  `12-13` char(6) DEFAULT NULL,
+  `12-13` varchar(15) DEFAULT NULL CHECK (`12-13` IN ('regular manicure', 'gel manicure', 'regular pedicure', 'gel pedicure')),
   `12-13-client` varchar(20) DEFAULT NULL,
-  `13-14` char(6) DEFAULT NULL,
+  `12-13-contact` char(11) DEFAULT NULL,
+  `13-14` varchar(15) DEFAULT NULL CHECK (`13-14` IN ('regular manicure', 'gel manicure', 'regular pedicure', 'gel pedicure')),
   `13-14-client` varchar(20) DEFAULT NULL,
-  `14-15` char(6) DEFAULT NULL,
+  `13-14-contact` char(11)  DEFAULT NULL,
+  `14-15` varchar(15) DEFAULT NULL CHECK (`14-15` IN ('regular manicure', 'gel manicure', 'regular pedicure', 'gel pedicure')),
   `14-15-client` varchar(20) DEFAULT NULL,
-  `15-16` char(6) DEFAULT NULL,
+  `14-15-contact` char(11)  DEFAULT NULL,
+  `15-16` varchar(15) DEFAULT NULL CHECK (`15-16` IN ('regular manicure', 'gel manicure', 'regular pedicure', 'gel pedicure')),
   `15-16-client` varchar(20) DEFAULT NULL,
-  `16-17` char(6) DEFAULT NULL,
+  `15-16-contact` char(11)  DEFAULT NULL,
+  `16-17` varchar(15) DEFAULT NULL CHECK (`16-17` IN ('regular manicure', 'gel manicure', 'regular pedicure', 'gel pedicure')),
   `16-17-client` varchar(20) DEFAULT NULL,
-  `17-18` char(6) DEFAULT NULL,
+  `16-17-contact` char(11)  DEFAULT NULL,
+  `17-18` varchar(15) DEFAULT NULL CHECK (`17-18` IN ('regular manicure', 'gel manicure', 'regular pedicure', 'gel pedicure')),
   `17-18-client` varchar(20) DEFAULT NULL,
+  `17-18-contact` char(11)  DEFAULT NULL,
   `bookingDate` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
